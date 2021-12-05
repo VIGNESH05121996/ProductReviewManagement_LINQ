ae72dea5c22f9fec6f53b13a4ca40b0c6287257b
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-12-05 11:05:13 +0530

    [VIGNESH M] Added: ProductReview Class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProductReviewManagement_LINQ
 {
@@ -7,6 +8,15 @@ namespace ProductReviewManagement_LINQ
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Product Review Management Using LINQ");
+
+            List<ProductReview> list = new List<ProductReview>()
+            {
+                 new ProductReview() { ProductId = 1, UserId = 1, Review = "good", Rating = 17, IsLike = true },
+                 new ProductReview() { ProductId = 2, UserId = 3, Review = "bad", Rating = 1, IsLike = false },
+                 new ProductReview() { ProductId = 3, UserId = 5, Review = "average", Rating = 20, IsLike = true },
+                 new ProductReview() { ProductId = 4, UserId = 7, Review = "good", Rating = 7, IsLike = false },
+                 new ProductReview() { ProductId = 5, UserId = 9, Review = "bad", Rating = 5, IsLike = true }
+            };
         }
     }
 }
