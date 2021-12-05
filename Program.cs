cd224f7b15065f90508789999abf16d135a4bdde
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-12-05 10:49:23 +0530

    [VIGNESH M] Added: Welcome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductReviewManagement_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management Using LINQ");
+        }
+    }
+}
